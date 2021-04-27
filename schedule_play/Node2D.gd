3b7edd868b21b2b96fb0120ccce3ef8f8e4b475f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-04-27 10:54:05 -0700

    Add demo.

---
+++ schedule_play/Node2D.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():	
+	var mix_time = AudioServer.get_last_mix_time()	
+	for i in range(0, 1000):
+		var ap = get_node("AudioStreamPlayer").duplicate()
+		add_child(ap)
+		ap.play_scheduled(_generate_time(mix_time, i))
+
+func _generate_time(mix_time, times):	
+	var interval_sec = .6
+	var next_audio_event = mix_time + interval_sec * times
+	print("Scheduling at mix time " + str(mix_time) + " at " + str(next_audio_event))
+	return next_audio_event
